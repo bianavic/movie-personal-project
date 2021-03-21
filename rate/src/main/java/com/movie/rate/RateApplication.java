b4adcd04751413a2a18d9d7f8a1bb26706e924e6
bianavic <bianavic@gmail.com>
2021-03-21 13:11:38 -0300

    converted services to eureka clients

--- rate/src/main/java/com/movie/rate/RateApplication.java
+++ rate/src/main/java/com/movie/rate/RateApplication.java
@@ -2,8 +2,10 @@ package com.movie.rate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RateApplication {
 
 	public static void main(String[] args) {
